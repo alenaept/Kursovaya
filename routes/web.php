99bf08adccf4d82285b54de15529866f41a4e8aa
23049 <chernilka20@mail.ru>
2026-06-24 06:37:20 +0800

    .

--- routes/web.php
+++ routes/web.php
@@ -39,9 +39,9 @@ Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    //Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    //Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    //Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::post('/user/reviews', [App\Http\Controllers\User\ReviewController::class, 'store'])->name('user.reviews.store');
 });
